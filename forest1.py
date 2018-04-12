2b547f86fae4e0393528f1ef27f7847f38a04ac6
LOW Zhi Hao <zhlow2@ad.cityu.edu.hk>
2018-04-12 21:38:17 +0800

    agasg

--- forest1.py
+++ forest1.py
@@ -10,8 +10,10 @@ from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 label = le.fit_transform(training_labels)
 
-clf = tree.DecisionTreeClassifier(max_depth=4, criterion="entropy")
-clf = clf.fit(mydataframe, label)
+from sklearn.ensemble import RandomForestClassifier
+
+clf = RandomForestClassifier(n_estimators = 3)
+# clf = clf.fit(mydataframe, label)
 
 import graphviz
 dot_data = tree.export_graphviz(clf, out_file=None,
@@ -31,15 +33,18 @@ prediction = clf.predict(myTestData)
 target = myTest.iloc[:,0]
 target = le.fit_transform(target)
 
+
+print(clf.estimators_)
+
 from sklearn.metrics import accuracy_score, confusion_matrix
 print (accuracy_score(target,prediction))
 print (confusion_matrix(target,prediction))
 
-for i in range(len(prediction))  :
-    if prediction[i] != target[i]:
-        print(clf.decision_path(myTest).todense()[i])
-        print('class:'+ target[i])
-        print('predict:' + prediction[i])
+# for i in range(len(prediction))  :
+#     if prediction[i] != target[i]:
+#         print(clf.decision_path(myTest).todense()[i])
+#         print('class:'+ target[i])
+#         print('predict:' + prediction[i])
 
 
 
