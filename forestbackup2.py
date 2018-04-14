3a8560d32074e90e7a7a86a879b1e6ddfe757ca1
lowzhao <lowzhao@gmail.com>
2018-04-14 23:23:15 +0800

    aupdated q3

--- forestbackup2.py
+++ forestbackup2.py
@@ -22,50 +22,50 @@ import numpy as np
 highestN = 0
 highestScore = 0
 
-for x in range(0,10000):
-	# n=100
-	n = int(math.floor(random.random()*40) + 1)
-	clf = RandomForestClassifier(n_estimators = n)
-	# clf = GaussianNB()
-	clf = clf.fit(mydataframe, label)
+# for x in range(0,10000):
+n=100
+# n = int(math.floor(random.random()*40) + 1)
+clf = RandomForestClassifier(n_estimators = n)
+# clf = GaussianNB()
+clf = clf.fit(mydataframe, label)
 
-	# import graphviz
+# import graphviz
 
-	# for x in range(0,n):
-	# 	dot_data = tree.export_graphviz(clf.estimators_[x], out_file=None,
-	# 	feature_names = mydataframe.columns,
-	# 	class_names=["Sugi","Hinoki","mixed deciduous","non-forest land"],
-	# 	filled=True, rounded=True,
-	# 	special_characters=True)
+for x in range(0,n):
+	dot_data = tree.export_graphviz(clf.estimators_[x], out_file=None,
+	feature_names = mydataframe.columns,
+	class_names=["Sugi","Hinoki","mixed deciduous","non-forest land"],
+	filled=True, rounded=True,
+	special_characters=True)
 
-	# 	graph = graphviz.Source(dot_data)
-	# 	graph.view('image' + str(x))
-
-
+	graph = graphviz.Source(dot_data)
+	graph.view('image' + str(x))
 
 
 
-	myTest = pd.read_csv('testing.csv')
-	myTestData = myTest.iloc[:,1:28]
-	prediction = clf.predict(myTestData)
-	target = myTest.iloc[:,0]
-	target = le.fit_transform(target)
-
-	# print(clf.estimators_)
-
-	# print (accuracy_score(target,prediction))
-	# print (confusion_matrix(target,prediction))
-
-	# print(mydataframe.target_names[prediction])
 
 
-	
-	# if highestScore < accuracy_score(target,prediction):
-	# 	highestN = n
-	# 	highestScore = accuracy_score(target,prediction)
-	tempScore = accuracy_score(target,prediction)
-	if tempScore > 0.84:
-		print(n)
+myTest = pd.read_csv('testing.csv')
+myTestData = myTest.iloc[:,1:28]
+prediction = clf.predict(myTestData)
+target = myTest.iloc[:,0]
+target = le.fit_transform(target)
+
+# print(clf.estimators_)
+
+# print (accuracy_score(target,prediction))
+# print (confusion_matrix(target,prediction))
+
+# print(mydataframe.target_names[prediction])
+
+
+
+# if highestScore < accuracy_score(target,prediction):
+# 	highestN = n
+# 	highestScore = accuracy_score(target,prediction)
+tempScore = accuracy_score(target,prediction)
+if tempScore > 0.84:
+	print(n)
 		
 
 
