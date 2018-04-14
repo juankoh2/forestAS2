fce4a7193520875c2fb61c32076d479985c8ebf2
juankoh2 <kohwanjuan@yahoo.com>
2018-04-14 19:09:13 +0800

    fvf

--- forest1.py
+++ forest1.py
@@ -1,7 +1,7 @@
 from sklearn import tree
 
 import pandas as pd
-mydata= pd.read_csv('testing.csv')
+mydata= pd.read_csv('training.csv')
 
 training_labels = mydata.iloc[:,0]
 mydataframe = mydata.iloc[:, 1:28]
@@ -13,40 +13,61 @@ label = le.fit_transform(training_labels)
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 
-n = 3;
 
-clf = RandomForestClassifier(n_estimators = n, max_depth=2)
-# clf = GaussianNB()
-clf = clf.fit(mydataframe, label)
-
-import graphviz
-
-for x in range(0,n):
-	dot_data = tree.export_graphviz(clf.estimators_[x], out_file=None,
-	feature_names = mydataframe.columns,
-	class_names=["Sugi","Hinoki","mixed deciduous","non-forest land"],
-	filled=True, rounded=True,
-	special_characters=True)
-
-	graph = graphviz.Source(dot_data)
-	graph.view('image' + x)
-
-
-
-
+import math
+import random
+from sklearn.metrics import accuracy_score, confusion_matrix
 import numpy as np
 
-myTest = pd.read_csv('training.csv')
-myTestData = myTest.iloc[:,1:28]
-prediction = clf.predict(myTestData)
-target = myTest.iloc[:,0]
-target = le.fit_transform(target)
+highestN = 0
+highestScore = 0
 
-print(clf.estimators_)
+for x in range(0,1):
+	# n=100
+	n = math.floor(random.random()*1000) + 1
+	
+	clf = RandomForestClassifier(n_estimators = n)
+	# clf = GaussianNB()
+	clf = clf.fit(mydataframe, label)
 
-from sklearn.metrics import accuracy_score, confusion_matrix
-print (accuracy_score(target,prediction))
-print (confusion_matrix(target,prediction))
+	# import graphviz
 
-# print(mydataframe.target_names[prediction])
+	# for x in range(0,n):
+	# 	dot_data = tree.export_graphviz(clf.estimators_[x], out_file=None,
+	# 	feature_names = mydataframe.columns,
+	# 	class_names=["Sugi","Hinoki","mixed deciduous","non-forest land"],
+	# 	filled=True, rounded=True,
+	# 	special_characters=True)
 
+	# 	graph = graphviz.Source(dot_data)
+	# 	graph.view('image' + str(x))
+
+
+
+
+
+	myTest = pd.read_csv('testing.csv')
+	myTestData = myTest.iloc[:,1:28]
+	prediction = clf.predict(myTestData)
+	target = myTest.iloc[:,0]
+	target = le.fit_transform(target)
+
+	# print(clf.estimators_)
+
+	print (accuracy_score(target,prediction))
+	print (confusion_matrix(target,prediction))
+
+	# print(mydataframe.target_names[prediction])
+
+
+	
+	if highestScore < accuracy_score(target,prediction):
+		highestN = n
+		highestScore = accuracy_score(target,prediction)
+		
+
+
+print(highestN)
+print(highestScore)
+
+# print(clf.feature_importances_)
